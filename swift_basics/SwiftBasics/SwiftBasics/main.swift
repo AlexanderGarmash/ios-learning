a63fda3f8ecf6a28cf48c7f3cadd4631e8ee61d7
Alexander Garmash <sttwo2@gmail.com>
2015-11-05 17:08:20 +0300

    Added some examples of usage swift

--- swift_basics/SwiftBasics/SwiftBasics/main.swift
+++ swift_basics/SwiftBasics/SwiftBasics/main.swift
@@ -8,5 +8,27 @@
 
 import Foundation
 
+let myConst: Float = 4.0
+var someFuckingList = [String]()
+let printString: String = "My constant is: \(myConst)"
+let someFuckingDictonary: [Int: String] = [
+    0: "First",
+]
+
+//Add some fucking values
+someFuckingList.append("Some fucking value 1")
+someFuckingList.append("Some fucking value 2")
+
+var someFuckingString: String = "My fucking list has \(someFuckingList.count) elements"
+
+//print some fucking strings
 print("Hello, World!")
+print(printString)
+print(someFuckingString)
+
+for (index, str) in someFuckingList.enumerate() {
+    if let enumName = someFuckingDictonary[index]?.lowercaseString {
+        print("The \(enumName) is \"\(str)\"")
+    }
+}
 
